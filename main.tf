5e9a645ac819737cb65f09535a4ff8c3fab911eb
bpsod10 <52925521+bpsod10@users.noreply.github.com>
2023-11-06 11:07:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
 }
+
+
+resource "aws_instance" "example" {
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = t2.micro
+    instance_type = "t2.micro"
+    }
