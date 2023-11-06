ce4e322f689e2f42b4fa379773eaf628013ca13d
bpsod10 <52925521+bpsod10@users.noreply.github.com>
2023-11-06 11:05:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
   backend "s3" {
     bucket         = "ed-eos-terraform-state-bod2"
     region         = "us-east-1"
     key            = "dev/state/terraform.tfstate"
     dynamodb_table = "eos_table2"
   }
+}
 
-resource "aws_instance" "example" {
-    ami = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-    }
+provider "aws" {
+  region  = "us-east-1"
+}
